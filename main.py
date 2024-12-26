126e39ced9ee2a5e330ac68c59e736aa004f3f81
Zloybanan4ik <denik55557777809@gmail.com>
2024-12-27 02:41:46 +0300

    update gitignore

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 a = float(input("Input a: "))
 b = float(input("Input b: "))
-s = input('Input "+" or "-": ' )
+s = input('Input "+", "-", "*", or "/": ' )
 
 if s == '-':
     print(a-b)
 elif s == '+':
     print(a+b)
+elif s == '*':
+    print(a*b)
+elif s == '/':
+    if b != 0:
+        print(a/b)
+    else:
+        print("Division by zero is not allowed!")
 else:
     print('Invalid type operation!!!')
