d12c8279760effdccec9b3310967bdfa5c9644a5
Zloybanan4ik <denik55557777809@gmail.com>
2024-12-27 02:57:33 +0300

    Логарифм

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import math
+
 a = float(input("Input a: "))
-s = input('Input "+", "-", "*", "/", "^", "sqrt", or "abs": ' )
+s = input('Input "+", "-", "*", "/", "^", "sqrt", "abs", or "log": ' )
 
 if s == '-':
     b = float(input("Input b: "))
@@ -23,5 +25,10 @@ elif s == 'sqrt':
     print(math.sqrt(a))
 elif s == 'abs':
     print(abs(a))
+elif s == 'log':
+    if a > 0:
+        print(math.log(a))
+    else:
+        print("Logarithm of non-positive number is not defined!")
 else:
     print('Invalid type operation!!!')
