cdae12b13477a1e47ce2969260890b79cb1035fd
Zloybanan4ik <denik55557777809@gmail.com>
2024-12-27 02:55:47 +0300

    Добавлено извлечение корня

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import math
+
 a = float(input("Input a: "))
-b = float(input("Input b: "))
-s = input('Input "+", "-", "*", "/", or "^": ' )
+s = input('Input "+", "-", "*", "/", "^", or "sqrt": ' )
 
 if s == '-':
+    b = float(input("Input b: "))
     print(a-b)
 elif s == '+':
+    b = float(input("Input b: "))
     print(a+b)
 elif s == '*':
+    b = float(input("Input b: "))
     print(a*b)
 elif s == '/':
+    b = float(input("Input b: "))
     if b != 0:
         print(a/b)
     else:
         print("Division by zero is not allowed!")
 elif s == '^':
+    b = float(input("Input b: "))
     print(a**b)
+elif s == 'sqrt':
+    print(math.sqrt(a))
 else:
     print('Invalid type operation!!!')
